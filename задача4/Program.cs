61db22339379b87feec7689d93c8ce32960d3729
BogdanBAE <bogdan.elistratov@gmail.com>
2022-10-04 13:34:38 +0400

    Написал вторую задачу

---
+++ задача4/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.Write("Введите первое число a: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число b: ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число с: ");
+int c = int.Parse(Console.ReadLine());
+int max = a;
+
+if (b > max)
+{
+    max = b;
+} 
+if (c > max)
+{
+    max = c;
+}
+Console.Write($"Максимальное число = {max}");
