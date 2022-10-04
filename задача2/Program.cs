10cfedb983a0c30dad3c408fe885bb576ea7ee55
BogdanBAE <bogdan.elistratov@gmail.com>
2022-10-04 13:14:19 +0400

    Написал первую задачу

---
+++ задача2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int firstNumber = int.Parse(Console.ReadLine());
+Console.Write("ВВедите второе число: ");
+int secondNumber = int.Parse(Console.ReadLine());
+
+if (firstNumber > secondNumber)
+{
+    Console.Write($"Max = {firstNumber} MIN =  {secondNumber}");
+} 
+else
+{
+    Console.Write($"Max = {secondNumber} MIN =  {firstNumber}");
+}
