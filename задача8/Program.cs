c9e63e75885891afa1fb88d4109d66e314168926
BogdanBAE <bogdan.elistratov@gmail.com>
2022-10-04 15:00:40 +0400

    Написал 4  задачу

---
+++ задача8/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.Write("Введите положительное число: ");
+int N = int.Parse(Console.ReadLine());
+
+if (N > 0)
+{
+    for (int i = 2; i <= N; i++) 
+    {
+        if (i % 2 == 0)
+        {
+            Console.Write($" {i} ");
+        }
+    }
+}
+else 
+    {
+        Console.Write("Введено отрицательное значение");
+    }
